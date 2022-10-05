ecb5845f1bb1deb3905e8cfc56892e60ca89f28f
Alex Linden <alextl2004@gmail.com>
2022-10-05 12:16:47 -0700

    get route for all cupcakes and cupcake model

--- app.py
+++ app.py
@@ -8,9 +8,24 @@ from models import db, connect_db, Cupcake, DEFAULT_IMAGE
 app = Flask(__name__)
 
 #TODO: rename
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///______'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cupcakes2'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = "oh-so-secret"
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
+
+@app.get("/")
+def root():
+    """Render homepage."""
+
+    return render_template("index.html")
+
+@app.get("/api/cupcakes")
+def root()
+    """Return info on all cupcakes like
+    {cupcakes: [{id, flavor, size, rating, image}, ...]}"""
+
+    cupcakes = [cupcake.serialize() for cupcake in Cupcake.query.all()]
+    return jsonify(cupcakes=cupcakes)
+
